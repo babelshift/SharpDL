4e85869dd9de9978c028bebfb545ffbce667c368
Justin Skiles <justin.d.skiles@gmail.com>
2013-09-09 22:27:22 -0400

    Added rectangle properties and methods. Added Vector structure.

--- SharpDL.Graphics/Rectangle.cs
+++ SharpDL.Graphics/Rectangle.cs
@@ -18,6 +18,33 @@ namespace SharpDL.Graphics
 		public int Left { get { return X; } }
 		public int Right { get { return X + Width; } }
 
+		public static Rectangle Empty
+		{
+			get
+			{
+				return new Rectangle()
+				{
+					Height = 0,
+					Width = 0,
+					X = 0,
+					Y = 0
+				};
+			}
+		}
+
+		public bool IsEmpty
+		{
+			get
+			{
+				if (Width == 0 && Height == 0)
+					return true;
+				else
+					return false;
+			}
+		}
+
+		public Point Location { get { return new Point((float)X, (float)Y); } }
+
 		public Rectangle(int x, int y, int width, int height)
 			: this()
 		{
@@ -25,6 +52,35 @@ namespace SharpDL.Graphics
 			Y = y;
 			Width = width;
 			Height = height;
+		}
+
+		public bool Contains(Point point)
+		{
+			if (point.X >= Left && point.X <= Right && point.Y >= Top && point.Y <= Bottom)
+				return true;
+			else
+				return false;
+		}
+
+		public bool Contains(Rectangle rectangle)
+		{
+			if (rectangle.Left >= Left && rectangle.Right <= Right && rectangle.Top >= Top && rectangle.Bottom <= Bottom)
+				return true;
+			else
+				return false;
+		}
+
+		public bool Intersects(Rectangle rectangle)
+		{
+			Point topLeft = new Point(rectangle.X, rectangle.Y);
+			Point bottomLeft = new Point(rectangle.X, rectangle.Bottom);
+			Point topRight = new Point(rectangle.Y, rectangle.Right);
+			Point bottomRight = new Point(rectangle.Right, rectangle.Bottom);
+
+			if(this.Contains(topLeft) || this.Contains(bottomLeft) || this.Contains(topRight) || this.Contains(bottomRight))
+				return true;
+			else
+				return false;
 		}
 	}
 }
---
+++ SharpDL.Graphics/Vector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpDL.Graphics
+{
+	public struct Vector
+	{
+		public float X { get; private set; }
+		public float Y { get; private set; }
+
+		public Vector One { get { return new Vector() { X = 1, Y = 1 }; } }
+		public Vector Zero { get { return new Vector() { X = 0, Y = 0 }; } }
+
+		public Vector(float x, float y) : this()
+		{
+			X = x;
+			Y = y;
+		}
+
+		public Vector Add(Vector vector)
+		{
+			float x = X + vector.X;
+			float y = Y + vector.Y;
+			return new Vector(x, y);
+		}
+
+		public static Vector operator +(Vector value1, Vector value2)
+		{
+			return value1.Add(value2);
+		}
+	}
+}

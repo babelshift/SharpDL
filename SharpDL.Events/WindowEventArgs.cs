d8606519dbfe72a6af3ec48a8802db5782811433
justin <justin.d.skiles@gmail.com>
2013-11-11 22:11:56 -0500

    Missed a file from last commit.

--- SharpDL.Events/WindowEventArgs.cs
+++ SharpDL.Events/WindowEventArgs.cs
@@ -7,28 +7,8 @@ using System.Threading.Tasks;
 
 namespace SharpDL.Events
 {
-	public enum WindowEventType
-	{
-		Shown = SDL.SDL_WindowEventID.SDL_WINDOWEVENT_SHOWN,
-		Hidden = SDL.SDL_WindowEventID.SDL_WINDOWEVENT_HIDDEN,
-		Exposed = SDL.SDL_WindowEventID.SDL_WINDOWEVENT_EXPOSED,
-		Moved = SDL.SDL_WindowEventID.SDL_WINDOWEVENT_MOVED,
-		Resized = SDL.SDL_WindowEventID.SDL_WINDOWEVENT_RESIZED,
-		SizeChanged = SDL.SDL_WindowEventID.SDL_WINDOWEVENT_SIZE_CHANGED,
-		Minimized = SDL.SDL_WindowEventID.SDL_WINDOWEVENT_MINIMIZED,
-		Maximized = SDL.SDL_WindowEventID.SDL_WINDOWEVENT_MAXIMIZED,
-		Restored = SDL.SDL_WindowEventID.SDL_WINDOWEVENT_RESTORED,
-		Enter = SDL.SDL_WindowEventID.SDL_WINDOWEVENT_ENTER,
-		Leave = SDL.SDL_WindowEventID.SDL_WINDOWEVENT_LEAVE,
-		FocusGained = SDL.SDL_WindowEventID.SDL_WINDOWEVENT_FOCUS_GAINED,
-		FocusLost = SDL.SDL_WindowEventID.SDL_WINDOWEVENT_FOCUS_LOST,
-		Close = SDL.SDL_WindowEventID.SDL_WINDOWEVENT_CLOSE
-	}
-
 	public class WindowEventArgs : GameEventArgs
 	{
-
-
 		public int Data1 { get; private set; }
 
 		public int Data2 { get; private set; }

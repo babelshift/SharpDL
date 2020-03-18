edec08c23263177553aba4364cd10eae51aed194
Justin <justin.d.skiles@gmail.com>
2020-03-18 12:40:05 -0400

    Updated to .NET Standard 2.0 and created an example with it.

--- SharpDL.Events/TextInputEventArgs.cs
+++ SharpDL.Events/TextInputEventArgs.cs
@@ -23,13 +23,13 @@ namespace SharpDL.Events
 			byte[] rawBytes = new byte[SDL2.SDL.SDL_TEXTINPUTEVENT_TEXT_SIZE];
 
 			// we have an unsafe pointer to a char array from SDL, explicitly marshal this to a byte array of fixed size
-			unsafe { Marshal.Copy((IntPtr)rawEvent.text.text, rawBytes, 0, SDL2.SDL.SDL_TEXTINPUTEVENT_TEXT_SIZE); }
+			unsafe { Marshal.Copy((IntPtr)rawEvent.text.text, rawBytes, 0, SDL.SDL_TEXTINPUTEVENT_TEXT_SIZE); }
 
 			// look for the index of the first null terminator
 			int length = Array.IndexOf(rawBytes, (byte)0);
 
 			// according to the SDL2 migration guide, TextInputEvent will have a UTF-8 encoded string
-			Text = System.Text.Encoding.UTF8.GetString(rawBytes, 0, length);
+			Text = Encoding.UTF8.GetString(rawBytes, 0, length);
 		}
 	}
 }

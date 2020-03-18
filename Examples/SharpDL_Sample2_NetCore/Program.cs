1c033ecffa063c5c3cc8b8a466c84742e850bb4f
Justin Skiles <justin.d.skiles@gmail.com>
2020-03-18 12:48:50 -0400

    Feature/update to net core (#4)
    * Updated to .NET Standard 2.0 and created an example with it.
    
    * Attempting to fix CI
    
    * Use VS2019 build image for CI

---
+++ Examples/SharpDL_Sample2_NetCore/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SharpDL_Sample2_NetCore
+{
+    class Program
+    {
+        [STAThread()]
+        static void Main(string[] args)
+        {
+            MainGame game = new MainGame();
+            game.Run();
+        }
+    }
+}
